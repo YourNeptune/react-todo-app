e38c3823227080791acb2ba4faecadddf5163e78
Lixuan Luo <lixuanluo@hotmail.com>
2021-04-06 23:54:45 -0400

    update

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,4 @@
-import firebase from 'firebase'
-
-const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyDB3ALxk1ZZ7pqGeWCFEMgqFNbJjX6GdoI",
-  authDomain: "react-todo-app-35d00.firebaseapp.com",
-  projectId: "react-todo-app-35d00",
-  storageBucket: "react-todo-app-35d00.appspot.com",
-  messagingSenderId: "866058771461",
-  appId: "1:866058771461:web:2eab0a6850df5da819906c",
-  measurementId: "G-KKJ8GZ266D"
-})
+import {firebaseApp} from './firebaseConfig'
 
 const db = firebaseApp.firestore()
 
